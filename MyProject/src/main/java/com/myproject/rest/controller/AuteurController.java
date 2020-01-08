1e0d861a0a55da9ec332e62049cfdec35c0a138a
MPRSOPRA <marc.pescher@soprasteria.com>
2020-01-08 14:31:48 +0100

    ajout du stub

--- MyProject/src/main/java/com/myproject/rest/controller/AuteurController.java
+++ MyProject/src/main/java/com/myproject/rest/controller/AuteurController.java
@@ -1,16 +1,18 @@
 package com.myproject.rest.controller;
 
-import java.net.URI;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.myproject.rest.dao.AuteurDAO;
 import com.myproject.rest.model.Auteur;
@@ -18,30 +20,36 @@ import com.myproject.rest.model.Auteur;
 @RestController
 @RequestMapping(path = "/auteurs")
 public class AuteurController {
-	
+
 	@Autowired
-    private AuteurDAO auteurDao;
-     
-    @GetMapping(path="/", produces = "application/json")
-    public List<Auteur> getAuteurs() 
-    {
-        return auteurDao.getAllAuteurs();
-    }
-     
-    @PostMapping(path= "/", consumes = "application/json", produces = "application/json")
-    public ResponseEntity<Object> addAuteur(@RequestBody Auteur auteur) 
-    {
-        Integer id = auteurDao.getAllAuteurs().size() + 1;
-        auteur.setId(id.longValue());
-         
-        auteurDao.addAuteur(auteur);
-         
-        URI location = ServletUriComponentsBuilder.fromCurrentRequest()
-                                    .path("/{id}")
-                                    .buildAndExpand(auteur.getId())
-                                    .toUri();
-         
-        return ResponseEntity.created(location).build();
-    }
+	private AuteurDAO auteurDao;
+
+	@GetMapping
+	public List<Auteur> findAll() {
+		return auteurDao.findAll();
+	}
+
+	@GetMapping(value = "/{id}")
+	public Auteur findById(@PathVariable("id") final Long id) {
+		return auteurDao.findById(id);
+	}
+
+	@PostMapping
+	@ResponseStatus(HttpStatus.CREATED)
+	public Long create(@RequestBody final Auteur resource) {
+		return auteurDao.create(resource);
+	}
+
+	@PutMapping(value = "/{id}")
+	@ResponseStatus(HttpStatus.OK)
+	public void update(@PathVariable("id") final Long id, @RequestBody final Auteur resource) {
+		auteurDao.update(resource);
+	}
+
+	@DeleteMapping(value = "/{id}")
+	@ResponseStatus(HttpStatus.OK)
+	public void delete(@PathVariable("id") final Long id) {
+		auteurDao.deleteById(id);
+	}
 
 }
--- MyProject/src/main/java/com/myproject/rest/dao/AuteurDAO.java
+++ MyProject/src/main/java/com/myproject/rest/dao/AuteurDAO.java
@@ -7,26 +7,58 @@ import org.springframework.stereotype.Repository;
 
 import com.myproject.rest.model.Auteur;
 
-
- 
 @Repository
-public class AuteurDAO 
-{
-    private static List<Auteur> list = new ArrayList<>();
-     
-    static
-    {
-        list.add(new Auteur(1L, "Lokesh", "Gupta"));
-        list.add(new Auteur(2L, "Alex", "Kolenchiskey"));
-        list.add(new Auteur(3L, "David", "Kameron"));
-    }
-     
-    public List<Auteur> getAllAuteurs() 
-    {
-        return list;
-    }
-     
-    public void addAuteur(Auteur auteur) {
-        list.add(auteur);
-    }
+public class AuteurDAO {
+	private static List<Auteur>	auteurs	= new ArrayList<>();
+
+	Long						freeId	= 4L;
+
+	static {
+		auteurs.add(new Auteur(1L, "Lokesh", "Gupta"));
+		auteurs.add(new Auteur(2L, "Alex", "Kolenchiskey"));
+		auteurs.add(new Auteur(3L, "David", "Kameron"));
+	}
+
+	public List<Auteur> findAll() {
+		return auteurs;
+	}
+
+	public Auteur findById(final Long id) {
+
+		for (Auteur auteur : auteurs) {
+			if (auteur.getId().equals(id)) {
+				return auteur;
+			}
+		}
+
+		return null;
+	}
+
+	public Long create(final Auteur resource) {
+
+		Long id = freeId++;
+		resource.setId(id);
+		auteurs.add(resource);
+
+		return id;
+	}
+
+	public void update(final Auteur resource) {
+
+		deleteById(resource.getId());
+		auteurs.add(resource);
+	}
+
+	public void deleteById(final Long id) {
+
+		List<Auteur> rmLst = new ArrayList<>();
+
+		for (Auteur auteur : auteurs) {
+			if (auteur.getId().equals(id)) {
+				rmLst.add(auteur);
+			}
+		}
+
+		auteurs.removeAll(rmLst);
+	}
 }
